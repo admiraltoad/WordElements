71578c0c95474af6cee58f1c17494049231e3110
patillac <patillac@gmail.com>
2017-08-08 14:46:10 -0300

    Input can now accept sequences

--- WordElements.py
+++ WordElements.py
@@ -5,20 +5,55 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
+class TermsManager():
+    def __init__(self):
+        self.filePath = os.path.join(os.path.dirname(QApplication.arguments()[0]), "data.json")
+        self.data = {}
+        self.cache = []
+        self.load()
+
+    def load(self):
+        with open(self.filePath, 'r') as file:
+            self.data = json.load(file)
+
+    def save(self):
+        with open(self.filePath, 'w') as file:
+            json.dump(self.data, file)
+
+    def terms(self):
+        return list(self.data.keys())
+
+    def links(self, term):
+        try:
+            return self.data[term]
+        except:
+            return []
+
+    def add(self, term, link):
+        if term not in self.terms():
+            self.data[term] = [link]
+        else:
+            if link not in self.data[term]:
+                self.data[term].append(link)
+
+    def sequences(self, count=1, length=4, cached=False):
+        # count determines how many to generate
+        # length determines how long each one must be
+        # cached determines if we can return sequences that have been returned in previous calls
+        pass
+
 class FilterModel(QSortFilterProxyModel):
-    def __init__(self, term):
+    def __init__(self, filterText):
         super().__init__()
-        self.term = term.lower()
+        self.filterText = filterText.lower()
 
     def filterAcceptsRow(self, row, parent):
-        text = self.sourceModel().createIndex(row, 0, parent).data(Qt.EditRole)
-        return text.startswith(self.term)
+        return self.sourceModel().createIndex(row, 0, parent).data(Qt.EditRole).startswith(self.filterText)
 
 class ListModel(QAbstractTableModel):
-    def __init__(self, data, name):
+    def __init__(self, data):
         super().__init__()
         self.data = data
-        self.name = name
 
     def rowCount(self, parent=QModelIndex()):
         return len(self.data)
@@ -30,7 +65,7 @@ class ListModel(QAbstractTableModel):
         if role == Qt.DisplayRole:
             return str(self.data[index.row()]).title()
         elif role == Qt.EditRole:
-            return str(self.data[index.row()])
+            return str(self.data[index.row()]).lower()
         elif role == Qt.TextAlignmentRole:
             return Qt.AlignCenter
 
@@ -44,36 +79,27 @@ class ListView(QTableView):
         self.verticalHeader().hide()
         self.setStyleSheet('QTableView { font-weight: bold; font-size: 18pt; }')
 
-class Mainframe(QWidget):  
-    
-    compoundNounFilePath = None
-    compoundNounData = {}
-
+class Mainframe(QWidget):
     def __init__(self):
         super().__init__()  
-        self.dataFilePath = ''
+        self.termsManager = TermsManager()
         self.initLayout()        
         self.setWindowTitle('Word Elements')
         self.resize(800, 624)
         self.center()
         self.show()
+        self.updateTermsModel()
+
+        # related to generating random sequences. will be removed once I make a 
         self.counter = 1
         self.cached = []        
 
-        args = QApplication.arguments()
-        self.compoundNounFilePath = os.path.join(os.path.dirname(args[0]), "data.json") 
-        self.loadJSONFile(self.compoundNounFilePath)
-        self.updateTermsModel()
-
     def createFileInputLayout(self):
         label = QLabel('Word Database:')
-        self.fileInput = QLineEdit(self.dataFilePath)
-        select = QPushButton('Select')
-        select.clicked.connect(self.selectDataFile)
+        filePath = QLineEdit(self.termsManager.filePath)
         hbox = QHBoxLayout()
         hbox.addWidget(label)
-        hbox.addWidget(self.fileInput)
-        hbox.addWidget(select)
+        hbox.addWidget(filePath)
         return hbox
 
     def createTermLayout(self):       
@@ -111,19 +137,6 @@ class Mainframe(QWidget):
         geom.moveCenter(QDesktopWidget().availableGeometry().center())
         self.move(geom.topLeft())
 
-    def selectDataFile(self):
-        filePath, _ = QFileDialog.getOpenFileName(self, 'Select a WordElements data file', '', 'JSON Files (*.json);;All Files (*)')
-        if filePath:
-            self.loadJSONFile(filePath)
-
-    def loadJSONFile(self, filepath): 
-        with open(filepath, 'r') as file:
-            self.compoundNounData = json.load(file)
-            self.dataFilePath = filepath
-            self.fileInput.setText(self.dataFilePath)
-            self.updateTermsModel()
-
-
     def termSelectionChanged(self, selected, deselected):
         indexes = selected.indexes()
         if len(indexes) <= 0:
@@ -131,27 +144,33 @@ class Mainframe(QWidget):
         else:
             self.updateLinksModel(indexes[0].data(Qt.EditRole))
 
-    def termInputTextChanged(self, text):        
-        try:
-            terms = text.split(' ')
+    def termInputTextChanged(self, text):  
+        terms = text.split(' ')
+        if len(terms) >=2 and not terms[-1]:
+            self.updateTermsModel(terms[-2])
+            self.updateLinksModel(terms[-2], None)
+        elif len(terms) >= 2:
+            self.updateTermsModel(terms[-2])
+            self.updateLinksModel(terms[-2], terms[-1])
+        elif len(terms) == 1:
             self.updateTermsModel(terms[0])
-            self.updateLinksModel(terms[0], terms[1])
-        except:
-            pass
+            self.updateLinksModel(terms[0], None)
+        else:
+            self.updateTermsModel(None)
+            self.updateLinksModel(None, None)
 
-    def termInputReturnPressed(self):        
-        try:
-            terms = self.termInput.text().split(' ')
-            if terms[1] not in self.compoundNounData[terms[0]]:
-                self.compoundNounData[terms[0]].append(terms[1])
-            self.termInput.setText('')
-        except:
-            pass
+    def termInputReturnPressed(self):     
+        terms = [term for term in self.termInput.text().split(' ') if term]
+        if len(terms) < 2:
+            return
+        for i in range(len(terms) - 1):
+            self.termsManager.add(terms[i].lower(), terms[i + 1].lower())
+        self.termInput.setText('')
 
     def updateTermsModel(self, filterText=None):
-        keys = list(self.compoundNounData.keys())
-        keys.sort()
-        model = ListModel(keys, 'Terms')
+        terms = self.termsManager.terms()
+        terms.sort()
+        model = ListModel(terms)
         if filterText:
             filter = FilterModel(filterText)
             filter.setSourceModel(model)
@@ -160,37 +179,38 @@ class Mainframe(QWidget):
         self.terms.selectionModel().selectionChanged.connect(self.termSelectionChanged)
 
     def updateLinksModel(self, term=None, filterText=None):
-        model = None
-        if term in self.compoundNounData:
-            links = self.compoundNounData[term]
+        if not term:
+            self.links.setModel(None)
+        else:
+            links = self.termsManager.links(term)
             links.sort()
-            model = ListModel(links, 'Links')
+            model = ListModel(links)
             if filterText:
                 filter = FilterModel(filterText)
                 filter.setSourceModel(model)
                 model = filter
-        self.links.setModel(model)  
+            self.links.setModel(model)
         
     def generateTestClicked(self):
-        terms = list(self.compoundNounData.keys())
+        terms = self.termsManager.terms()
         try:
             i = random.randint(0, len(terms))
             first = terms[i]
-            links = self.compoundNounData[first]
+            links = self.termsManager.links(first)
             useful = []
             for link in links:
                 if link in terms:
                     useful.append(link)
             i = random.randint(0, len(useful))
             second = useful[i]
-            links = self.compoundNounData[second]
+            links = self.termsManager.links(second)
             useful = []
             for link in links:
                 if link in terms:
                     useful.append(link)
             i = random.randint(0, len(useful))
             third = useful[i]
-            links = self.compoundNounData[third]
+            links = self.termsManager.links(third)
             i = random.randint(0, len(links))
             fourth = links[i]
             sequence = '{} > {} > {} > {}'.format(first, second, third, fourth)
@@ -203,6 +223,10 @@ class Mainframe(QWidget):
             self.testOutput.setText('{}'.format(self.counter))
             self.counter += 1
             QTimer.singleShot(1, self.generateTestClicked)
+
+    def closeEvent(self, event):
+        self.termsManager.save()
+        super().closeEvent(event)
        
 if __name__ == '__main__':   
     QCoreApplication.setOrganizationName("Word Elements")
